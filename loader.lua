f1219847e1fc055c8a951f0a681ef51c454e8872
korp <metabamgg@gmail.com>
2024-12-29 14:49:32 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,17 @@
 print("Loading Vision")
 --[ Gun Grounds FFA Script ]--
 if game.PlaceId == 12137249458 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/userlibs/Vision/refs/heads/main/src/visionffa.lua"))()
-  print("Gun Grounds Script Loading...")
---[ Universal Script ]--
-  if not
-loadstring(game:HttpGet("https://raw.githubusercontent.com/userlibs/Vision/refs/heads/main/src/vision.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/userlibs/Vision/refs/heads/main/src/visionffa.lua"))()
+    print("Gun Grounds Script Loading...")
+else
+    --[ Universal Script ]--
+    local success, err = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/userlibs/Vision/refs/heads/main/src/vision.lua"))()
+    end)
+    
+    if not success then
+        print("Failed to load Universal Script: " .. tostring(err))
+    else
+        print("Universal Script Loading...")
+    end
+end
