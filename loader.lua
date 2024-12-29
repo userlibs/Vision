83df5f301231d868e90ca6fb22788b239a56c733
korp <metabamgg@gmail.com>
2024-12-29 14:48:18 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,8 @@
 print("Loading Vision")
-
-wait(5)
-
+--[ Gun Grounds FFA Script ]--
+if game.PlaceId == 12137249458 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/userlibs/Vision/refs/heads/main/src/visionffa.lua"))()
+  print("Gun Grounds Script Loading...")
+--[ Universal Script ]--
+  if not
 loadstring(game:HttpGet("https://raw.githubusercontent.com/userlibs/Vision/refs/heads/main/src/vision.lua"))()
